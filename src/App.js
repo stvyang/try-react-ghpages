925dbebdb6ef22284341ff3c16e8e8ee9c27befb
stvyang <stv.yang10@gmail.com>
2018-06-18 23:03:52 +0700

    Try skip gh-pages branch build with changed website header name

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">
-            Welcome to React with CircleCI running only on master
+            Welcome to React with CircleCI which skips gh-pages
           </h1>
         </header>
         <p className="App-intro">
