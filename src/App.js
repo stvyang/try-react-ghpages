4eb3046e7eeed960c0a307c2cdf34cef840d69f4
stvyang <stv.yang10@gmail.com>
2018-06-19 00:14:38 +0700

    Rollback commit message to previous format

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">
-            Welcome to React with CircleCI with global gh-pages
-          </h1>
+          <h1 className="App-title">Welcome to React Final</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
