5628de55b020e804dbea41ce89ce47f20b9bb03e
stvyang <stv.yang10@gmail.com>
2018-06-18 23:47:57 +0700

    Try gh-pages branch commit message based on CIRCLE_SHA1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">
-            Welcome to React with CircleCI which skips gh-pages with commits
+            Welcome to React with CircleCI which skips gh-pages with dynamic
+            commits
           </h1>
         </header>
         <p className="App-intro">
